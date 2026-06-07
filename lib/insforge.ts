f78370a5953c1729d5f2a77d87d03218fc10a483
dbscjstk3 <dbscjstk3@gmail.com>
2026-06-07 14:44:41 +0900

    feat: add insforge client module

---
+++ lib/insforge.ts
@@ -0,0 +1,13 @@
+import { createClient } from '@insforge/sdk'
+
+export const insforge = createClient({
+  baseUrl: process.env.NEXT_PUBLIC_INSFORGE_URL!,
+  anonKey: process.env.NEXT_PUBLIC_INSFORGE_ANON_KEY!,
+})
+
+export interface Player {
+  id: string
+  real_name: string
+  summoner_name: string
+  created_at: string
+}
